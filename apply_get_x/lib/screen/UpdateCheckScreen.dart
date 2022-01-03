6decaa80ce7be7c5596081b50df1ad32f38ca27e
Riudiu <lacvert13@gmail.com>
2022-01-03 12:06:39 +0900

    22/01/03 - 데이터 호출하는 동안 로딩중 띄우기 / getx 라이브러리를 이용해 setState 없이 로딩중 띄워보기

--- apply_get_x/lib/screen/UpdateCheckScreen.dart
+++ apply_get_x/lib/screen/UpdateCheckScreen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,18 +12,40 @@ class UpdateCheckScreen extends StatefulWidget {
 
 class _UpdateCheckScreenState extends State<UpdateCheckScreen> {
   String url = "https://poodone.com/update.json";
+  String isUpdate = '';
+  String androidAppVersion = '';
+  String iosAppVersion = '';
+  bool loading = false;
+  RxBool rxLoading = false.obs;
 
   @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       appBar: AppBar(
         title: Text('업데이트 체크'),
       ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          body(),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            body(),
+            Text("업데이트: $isUpdate"),
+            Text("안드로이드: $androidAppVersion"),
+            Text("iOS: $iosAppVersion"),
+            // loading이 트루일때만 로딩중 띄우기
+            if (loading) CircularProgressIndicator(),
+
+            // setState를 사용하는 다른 방식 - build 다시 그리지않고 Obx에 해당하는 부분만 재실행할 수 있다.
+            Obx(() {
+              if (rxLoading.value) {
+                return CircularProgressIndicator();
+              } else {
+                return SizedBox();
+              }
+            }),
+          ],
+        ),
       ),
     );
   }
@@ -38,11 +60,29 @@ class _UpdateCheckScreenState extends State<UpdateCheckScreen> {
   }
 
   Future getUpdateData() async {
+    // 데이터 호출 전에
+    rxLoading.value = true;
+    // setState(() {
+    //   loading = true;
+    // });
+
+    // 고정값으로 딜레이시간 지정
+    await Future.delayed(Duration(seconds: 2));
     var response = await http.get(Uri.parse(url));
     print(response.body);
 
     print(json.decode(response.body));
     var data = json.decode(response.body);
+
     print(data["androidAppVersion"]);
+
+    // setState(() {
+    isUpdate = data["isUpdate"].toString();
+    androidAppVersion = data["androidAppVersion"];
+    iosAppVersion = data["iosAppVersion"];
+    loading = false;
+    // });
+    // setState 없이 사용가능
+    rxLoading.value = false;
   }
 }
